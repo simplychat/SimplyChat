3dfef3d5f670f3a4c2f0d768f1dd9bbb4fd272fd
natalie <neyclesley@gmail.com>
2019-02-12 22:48:45 -0500

    Add a textfield, create chat box

---
+++ lib/utils/constants.dart
@@ -0,0 +1,11 @@
+// Strings
+const String appName = 'ChatBee';
+
+// Margins
+const double materialMarginSmall = 12.0;
+const double materialMargin = 24.0;
+
+// Radii
+const double miniRadius = 5.0;
+const double smallRadius = 10.0;
+const double mediumRadius = 20.0;
